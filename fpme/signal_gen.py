bed8e0c50c5db6c0045235b6937de0c14994e63b
Philipp Holl <philipp.holl@tum.de>
2020-08-21 12:23:38 +0200

    Implement Motorola 2, fix Motorola 1

--- fpme/signal_gen.py
+++ fpme/signal_gen.py
@@ -1,7 +1,7 @@
 import threading
 import time
-import serial
 
+import serial
 
 TERNARY_BITS = [(63, 63), (0, 0), (0, 63)]  # 416 ms per bit
 T = TERNARY_BITS
@@ -13,7 +13,7 @@ def _all_addresses():
         for bit2 in TERNARY_BITS:
             for bit3 in TERNARY_BITS:
                 for bit4 in TERNARY_BITS:
-                    result.append(bit1 + bit2 + bit3 + bit4)
+                    result.append(bit4 + bit3 + bit2 + bit1)
     return tuple(result)
 
 
@@ -22,36 +22,48 @@ ALL_ADDRESSES = _all_addresses()
 
 class MaerklinProtocol:
 
-    def packet(self, address: int, speed: int, func: bool):
+    def packet(self, address: int, speed: int, reverse: bool, func: bool):
         """
         Generate the bytes for RS-232 to send a Märklin-Motorola message to a locomotive.
 
+        :param reverse: whether train is in reverse mode
         :param address: locomotove address between 1 and 80
         :param func: whether function is active
         :param speed: speed value: -14 to 14, 1 for direction change
         :return: package bytes
         """
-        packet = ALL_ADDRESSES[address] + (T[1] if func else T[0]) + self.velocity_bytes(speed)
+        packet = ALL_ADDRESSES[address] + (T[1] if func else T[0]) + self.velocity_bytes(speed, reverse)
+        print(packet)
         return bytes(packet)
 
-    def velocity_bytes(self, speed):
+    def velocity_bytes(self, speed: int, reverse: bool):
         raise NotImplementedError()
 
 
 class Motorola1(MaerklinProtocol):
 
-    def velocity_bytes(self, speed):
+    def velocity_bytes(self, speed: int, reverse: bool):
+        assert 0 <= speed <= 14
+        speed = speed + 1 if speed else 0
         return T[speed & 1] + T[(speed >> 1) & 1] + T[(speed >> 2) & 1] + T[speed >> 3]
 
 
 class Motorola2(MaerklinProtocol):
 
-    ALL_SPEEDS = [
-        # -14, ... 14
-    ]
-
-    def velocity_bytes(self, speed):
-        return self.ALL_SPEEDS[speed]  # TODO
+    def velocity_bytes(self, speed: int, reverse: bool):
+        assert 0 <= speed <= 14
+        if speed >= 7 and reverse:
+            b2, b4, b6, b8 = [1, 0, 1, 0]
+        elif speed <= 6 and reverse:
+            b2, b4, b6, b8 = [1, 0, 1, 1]
+        elif speed <= 6 and not reverse:
+            b2, b4, b6, b8 = [0, 1, 0, 1]
+        elif speed >= 7 and not reverse:
+            b2, b4, b6, b8 = [0, 1, 0, 0]
+        else:
+            raise ValueError()
+        bits = [speed & 1, b2, (speed >> 1) & 1, b4, (speed >> 2) & 1, b6, speed >> 3, b8]
+        return tuple(0 if b else 63 for b in bits)
 
 
 class SignalGenerator:
@@ -63,7 +75,7 @@ class SignalGenerator:
             parity=serial.PARITY_NONE,
             stopbits=serial.STOPBITS_ONE,
             bytesize=serial.SIXBITS,
-            write_timeout=0,  # non-blocking write
+            # write_timeout=0,  # non-blocking write
         )
         ser.is_open or ser.open()
         assert ser.is_open
@@ -84,7 +96,7 @@ class SignalGenerator:
                 pass  # TODO idle signal
             for address, (speed, func) in self.data.items():
                 for _rep in range(self.immediate_repetitions):
-                    packet = self.protocol.packet(address, speed, func)
+                    packet = self.protocol.packet(address, abs(speed), speed < 0, func)  # TODO emergency break through speed reversal?
                     self._ser.write(packet)
                     time.sleep(len(packet) * 208e-6)
                     time.sleep(1250e-6)  # >= 3 t-bits (6 bytes) pause between signals
@@ -98,6 +110,6 @@ class SignalGenerator:
 
 
 if __name__ == '__main__':
-    gen = SignalGenerator('COM1', Motorola1())
-    gen.set(78, 6, True)
+    gen = SignalGenerator('COM1', Motorola2())
+    gen.set(78, 8, False)
     gen.run()
