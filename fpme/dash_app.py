b03d3ee65074f174964f168e05bb7e15e4d67970
Philipp Holl <philipp.holl@tum.de>
2020-09-20 13:14:46 +0200

    Gauge for speed, vertical button layout, <-> button

--- fpme/dash_app.py
+++ fpme/dash_app.py
@@ -23,7 +23,17 @@ CLIENTS = {}
 INPUT_LOCK = threading.Lock()
 
 
-def client_input(name, accelerations, decelerations):
+CONTROL_UPDATE_INPUTS = [
+    Input('name', 'value'),
+    Input('accelerate', 'n_clicks'),
+    Input('decelerate', 'n_clicks'),
+    Input('reverse', 'n_clicks'),
+    Input('stop-train-placeholder', 'children'),
+    Input('main-update', 'n_intervals'),
+]
+
+
+def handle_control_input(name, accelerations, decelerations, reverses, stops, _):
     with INPUT_LOCK:
         if name not in logic.DRIVERS:
             return
@@ -37,6 +47,8 @@ def client_input(name, accelerations, decelerations):
             logic.accelerate(name, diff)
             client.accelerations = accelerations
             client.decelerations = decelerations
+    if not logic.can_control(name):
+        raise PreventUpdate()
 
 
 
@@ -82,22 +94,31 @@ tasks = html.Div(style={}, children=[
 )
 
 control_layout = html.Div(id='control', style={'display': 'none'}, children=[
-    html.H2('', id='train-name', style={'textAlign': 'center'}),
-    html.Div(style={'width': '100%', 'height': 60}, children=[
-        html.Button('-', id='decelerate', style={'width': '20%', 'height': '100%', 'display': 'inline-block', 'vertical-align': 'top'}),
-        html.Div(style={'width': '60%', 'height': '100%', 'display': 'inline-block', 'vertical-align': 'center'}, children=[
-            # dbc.Progress('Geschwindigkeit', value=50, color='success', className="mb-3", id='speed', style={'height': '100%'}),
-            daq.GraduatedBar(id='speed', value=1, min=-1, max=1.001, step=0.05, size=350,
-                             color={'gradient': True, 'ranges': {'gray': [-1, 0], 'green': [0, 0.4], 'yellow':[.4, .7], 'red':[.7, 1]}},
-                             style={'height': '100%'}),
+    # html.H2('', id='train-name', style={'textAlign': 'center'}),
+    html.Div(style={}, children=[
+        html.Div(style={'display': 'inline-block', 'vertical-align': 'top'}, children=[
+            daq.Gauge(id='speed', color={'gradient': True, 'ranges': {'green': [0, 6*25], 'yellow': [6*25, 8*25], 'red': [8*25, 10*25]}},
+                      value=250, label='Zug - Richtung', max=250, min=0, units='km/h', showCurrentValue=True, size=300),
         ]),
-        html.Button('+', id='accelerate', style={'width': '20%', 'height': '100%', 'display': 'inline-block', 'vertical-align': 'top'}),
+        html.Div(style={'display': 'inline-block', 'width': 80, 'height': 300, 'vertical-align': 'top'}, children=[
+            html.Div(style={'display': 'inline-block', 'width': '100%', 'height': '25%'}, children=[
+                html.Button('+', id='accelerate', style={'width': '100%', 'height': '100%', 'background-color': '#33cc33', 'color': 'white'}),
+            ]),
+            html.Div(style={'display': 'inline-block', 'width': '100%', 'height': '15%'}, children=[
+                html.Button('<->', id='reverse', style={'width': '100%', 'height': '100%', 'background-color': '#A0A0FF', 'color': 'white'}),
+            ]),
+            html.Div(style={'display': 'inline-block', 'width': '100%', 'height': '25%'}, children=[
+                html.Button('-', id='decelerate', style={'width': '100%', 'height': '100%', 'background-color': '#33cc33', 'color': 'white'}),
+            ]),
+            html.Div(style={'display': 'inline-block', 'width': '100%', 'height': '15%'}, children=[
+                html.Button('Nothalt', id='stop-train', style={'width': '100%', 'height': '100%', 'background-color': '#ff6600', 'color': 'white'}),
+            ]),
+            html.Div(style={'display': 'inline-block', 'width': '100%', 'height': '20%'}, children=[
+                html.Button('Strom', id='stop-all', style={'width': '100%', 'height': '100%', 'background-color': '#cc0000', 'color': 'white'}),
+            ]),
+        ]),
     ]),
     tasks,
-    html.Div(style={'width': '50%', 'height': 60, 'margin': 'auto'}, children=[
-        html.Button('Nothalt', id='stop-train', style={'width': '40%', 'height': '100%', 'display': 'inline-block', 'vertical-align': 'top', 'horizontal-align': 'left', 'background-color': 'purple', 'color': 'white'}),
-        html.Button('Strom aus', id='stop-all', style={'width': '40%', 'height': '100%', 'display': 'inline-block', 'vertical-align': 'top', 'horizontal-align': 'right', 'background-color': 'red', 'color': 'white'}),
-    ]),
     html.Div(id='stop-train-placeholder', style={'display': 'none'}),
     html.Div(id='stop-all-placeholder', style={'display': 'none'}),
 ])
@@ -125,70 +146,24 @@ def show_control(_n_intervals, name):
     raise PreventUpdate()
 
 
-@app.callback(Output('train-name', 'children'), [Input('main-update', 'n_intervals'), Input('name', 'value')])
-def set_train_name(_n_intervals, name):
-    if logic.can_control(name):
-        return logic.get_train_name(name)
-    raise PreventUpdate()
+@app.callback(Output('speed', 'label'), CONTROL_UPDATE_INPUTS)
+def set_train_name(name, *args):
+    handle_control_input(name, *args)
+    speed = logic.get_speed(name)
+    train = logic.get_train_name(name)
+    if speed == 0:
+        return train
+    if speed > 0:
+        return train + " - Vorwärts"
+    else:
+        return train + ' - Rückwärts'
 
 
-# @app.callback(Output('speed', 'value'), [Input('name', 'value'), Input('accelerate', 'n_clicks'), Input('decelerate', 'n_clicks'), Input('stop-train-placeholder', 'children')])
-# def update_speedometer(name, accelerations, decelerations, *args):
-#     client_input(name, accelerations, decelerations)
-#     if logic.can_control(name):
-#         speed = logic.get_speed(name)
-#         return int(round(abs(speed) * 100))
-#     raise PreventUpdate()
-#
-#
-# @app.callback(Output('speed', 'children'), [Input('name', 'value'), Input('accelerate', 'n_clicks'), Input('decelerate', 'n_clicks')])
-# def update_speedometer_text(name, accelerations, decelerations):
-#     client_input(name, accelerations, decelerations)
-#     if logic.can_control(name):
-#         speed = logic.get_speed(name)
-#         return '' if speed >= 0 else 'Rückwärts'
-#     raise PreventUpdate()
-#
-#
-# @app.callback(Output('speed', 'color'), [Input('name', 'value'), Input('accelerate', 'n_clicks'), Input('decelerate', 'n_clicks')])
-# def update_speedometer_color(name, accelerations, decelerations):
-#     client_input(name, accelerations, decelerations)
-#     if logic.can_control(name):
-#         speed = logic.get_speed(name)
-#         if abs(speed) > 0.95:
-#             return 'danger'
-#         if abs(speed) > 0.6:
-#             return 'warning'
-#         if speed > 0:
-#             return 'success'
-#         if speed < 0:
-#             return 'info'
-#     raise PreventUpdate()
-
-
-# @app.callback(Output('speed', 'color'), [Input('name', 'value'), Input('accelerate', 'n_clicks'), Input('decelerate', 'n_clicks')])
-# def update_speedometer_color(name, accelerations, decelerations):
-#     client_input(name, accelerations, decelerations)
-#     if logic.can_control(name):
-#         speed = logic.get_speed(name)
-#         print(speed)
-#         if speed == 0:
-#             return {'gradient': True, 'ranges': {'gray': [-1, 0], 'green': [0, 0.4], 'yellow': [.4, .7], 'red': [.7, 1]}}
-#             return {'ranges': {'white': [-1, 1]}}
-#         if speed > 0:
-#             return {'gradient': True, 'ranges': {'gray': [-1, 0], 'green': [0, 0.4], 'yellow': [.4, .7], 'red': [.7, 1]}}
-#         else:
-#             return {'gradient': True, 'ranges': {'gray': [0, 1], 'green': [-.4, 0], 'yellow': [-.7, -.4], 'red': [-1, -.7]}}
-#
-#         if abs(speed) > 0.95:
-#             return 'danger'
-#         if abs(speed) > 0.6:
-#             return 'warning'
-#         if speed > 0:
-#             return 'success'
-#         if speed < 0:
-#             return 'info'
-#     raise PreventUpdate()
+@app.callback(Output('speed', 'value'), CONTROL_UPDATE_INPUTS)
+def update_speedometer(name, *args):
+    handle_control_input(name, *args)
+    speed = logic.get_speed(name)
+    return int(round(abs(speed) * 250))
 
 
 @app.callback(Output('stop-train-placeholder', 'children'), [Input('stop-train', 'n_clicks'), Input('name', 'value')])
