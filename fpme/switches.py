b4c7de7a098a4ea9079916bc49781922f4a79f0e
Philipp Holl <philipp.holl@tum.de>
2022-08-11 13:07:31 +0200

    Improved switch fail states

--- fpme/switches.py
+++ fpme/switches.py
@@ -76,8 +76,11 @@ def set_all_locked(locked: bool):
 
 def _operate_switch(switch: int, curved: bool):
     """ Sends a signal to the specified track switch. """
-    print(f"Setting switch {switch} to state curved={curved}")
-    from .relay8 import pulse
-    channel = RELAY_CHANNEL_BY_SWITCH_STATE[switch][curved]
-    if pulse(channel):
-        STATES[switch] = curved
+    STATES[switch] = curved
+    try:
+        from .relay8 import pulse
+        channel = RELAY_CHANNEL_BY_SWITCH_STATE[switch][curved]
+        if not pulse(channel):
+            print(f"Failed to operate switch {switch} to state curved={curved}")
+    except BaseException as exc:
+        print(f"Failed to operate switch {switch}: {exc}")
