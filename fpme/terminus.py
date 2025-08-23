81d42ad86da545797dcb0d2494647c94409d422c
Philipp Holl <philipp@mholl.de>
2025-08-23 13:31:50 +0200

    Fix for obstacles

--- fpme/terminus.py
+++ fpme/terminus.py
@@ -606,8 +606,9 @@ def play_connections(platform: int, connections: List[Tuple[Train, int]]):
         connections = random.sample(connections, 2)
     texts = []
     for train, pl in connections:
-        name, target = TARGETS[train][pl]
-        texts.append(f"{name}, nach: {target} von Gleis {PL_NUM[pl]}{', direkt gegenüber' if OPPOSITE[platform] == pl else ''}.")
+        if train in TARGETS:
+            name, target = TARGETS[train][pl]
+            texts.append(f"{name}, nach: {target} von Gleis {PL_NUM[pl]}{', direkt gegenüber' if OPPOSITE[platform] == pl else ''}.")
     play_announcement(' '.join(texts), left_vol=int(platform <= 3), right_vol=int(platform > 3), cue='anschlüsse', cue_vol=1.)
     return 2 + 4.5 * len(texts)
 
